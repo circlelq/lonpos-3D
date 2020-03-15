bc7b6d6835d5bc1e0bf60c749ec3affa68730cad
circle <circle20101561@163.com>
2020-03-15 19:39:36 +0800

    添加说明

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 A *Lonpos* solving program that calculates all the possible 3D(pyramid) outcomes.
 
+![IMG_9466](lonpos.jpeg)
+
--- test.py
+++
@@ -1,8 +0,0 @@
-from numpy import *
-import math
-import os
-from functions import *
-import piecesinfo
-
-for i in Rotate3(Rotate3(piecesinfo.CYAN5)):
-	print(i)
